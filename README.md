b71ddf80607b0bf77b993ec734092adb1b4ad980
B-sirius <padfoot_07@163.com>
2017-08-04 21:18:19 +0800

    launch and drag

---
+++ README.md
@@ -0,0 +1,3 @@
+# Artificial Idiot
+
+兴趣使然的插件，可在网页上生成奇怪的东西，demo使用了名为 **annoying dog** 的可怕魔兽，出自 *Undertale*。
--- ai.js
+++ ai.js
@@ -4,6 +4,9 @@ let AI = function(imgSrc, style) {
     this.imgSrc = imgSrc;
     this.style = style;
 
+    this.xBase = 1;
+    this.yBase = 1;
+
     this._init();
 }
 
@@ -22,7 +25,52 @@ AI.prototype._init = function() {
         _self.el = el;
 
         _self.setStyle(this.style);
+
+        // 设定坐标系依据
+        if (this.style.right !== '') {
+            this.xKey = 'right';
+            this.xBase = -1;
+        } else {
+            this.xKey = 'left';
+            this.xBase = 1;
+        }
+        if (this.style.bottom !== '') {
+            this.yKey = 'bottom';
+            this.yBase = -1;
+        } else {
+            this.yKey = 'top';
+            this.yBase = 1;
+        }
+
+        // 插入dom
         document.body.insertBefore(el, document.body.firstChild);
+
+        // 绑定事件
+        let drag = throttleFn(_self.drag, 16, this);
+
+        let startDrag = function(e) {
+            e.preventDefault();
+
+            // 记录鼠标按下时的位置，注意这个位置是相对左上角的
+            _self.oldPos = {
+                x: e.clientX,
+                y: e.clientY
+            };
+            // 此时相关的style属性值
+            _self.oldStyle = {
+                x: parseFloat(_self.el.style[_self.xKey].slice(0, -2)),
+                y: parseFloat(_self.el.style[_self.yKey].slice(0, -2))
+            }
+
+            el.addEventListener('mousemove', drag);
+        }
+
+        let stopDrag = function() {
+            el.removeEventListener('mousemove', drag);
+        }
+
+        el.addEventListener('mousedown', startDrag);
+        document.body.addEventListener('mouseup', stopDrag);
     });
 }
 
@@ -33,7 +81,8 @@ AI.prototype.setStyle = function(style) {
 }
 
 AI.prototype.launch = function() {
-    let speed = 1;
+    let speed = 30; // 速度，帧为单位
+    let attrition = 5 / 60; // 速度损耗，同样帧为单位
     let angle = Math.random() * Math.PI * 2;
 
     let mapWidth = document.documentElement.clientWidth;
@@ -42,27 +91,78 @@ AI.prototype.launch = function() {
     let elWidth = parseFloat(this.el.style.width.slice(0, -2)); // 只考虑px
     let elHeight = parseFloat(this.el.style.height.slice(0, -2));
 
-    let xKey = this.style.right !== '' ? 'right' : 'left';
-    let yKey = this.style.bottom !== '' ? 'bottom' : 'top';
+    
 
-    let x = parseFloat(this.el.style[xKey].slice(0, -2));
-    let y = parseFloat(this.el.style[yKey].slice(0, -2));
+    let x = parseFloat(this.el.style[this.xKey].slice(0, -2));
+    let y = parseFloat(this.el.style[this.yKey].slice(0, -2));
 
-    let moveId = requestAnimationFrame(() => {
+    let _self = this;
+    let nextMove = function() {
+        // 每帧的位移
         let dX = speed * Math.cos(angle);
         let dy = speed * Math.sin(angle);
 
         x += dX;
         y += dy;
 
+        // 碰撞检测
         if (x < 0 || x > mapWidth - elWidth) {
-            angle = -angle;
+            angle = -angle + Math.PI;
+            x = x < 0 ? 0 : mapWidth - elWidth;
         }
 
         if (y < 0 || y > mapHeight - elHeight) {
-            angle = -angle + Math.PI;
+            angle = -angle;
+            y = y < 0 ? 0 : mapHeight - elHeight;
         }
-    });
+
+        // 设置样式
+        let style = {};
+        style[_self.xKey] = x + 'px';
+        style[_self.yKey] = y + 'px';
+        _self.setStyle(style);
+
+        // 速度损耗
+        speed -= attrition;
+        if (speed < 1) {
+            return;
+        }
+
+        requestAnimationFrame(nextMove);
+    }
+
+    requestAnimationFrame(nextMove);
+}
+
+AI.prototype.drag = function(e) {
+    let newPos = {
+        x: e.clientX,
+        y: e.clientY
+    };
+
+    // 移动的距离差，
+    let dx = newPos.x - this.oldPos.x;
+    let dy = newPos.y - this.oldPos.y;
+
+    let style = {};
+    style[this.xKey] = this.oldStyle.x + dx * this.xBase + 'px';
+    style[this.yKey] = this.oldStyle.y + dy * this.yBase + 'px';
+
+    this.setStyle(style);
+}
+
+let throttleFn = function(fn, minInterval, context) {
+    let timeoutId = null;
+
+    return function(e) {
+        if (timeoutId === null) {
+            fn.call(context, e);
+
+            timeoutId = setTimeout(() => {
+                timeoutId = null;
+            }, minInterval);
+        }
+    }
 }
 
 window.AI = AI;
